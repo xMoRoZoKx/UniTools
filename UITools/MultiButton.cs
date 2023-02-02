e0595cbec1bedbb2529d8a2fa95dc3ccc938997b
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-02-02 20:44:38 +0200

    add multiButtom

---
+++ UITools/MultiButton.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class MultiButtonAction
+{
+    public string name;
+    public UnityEvent onClick = new UnityEvent();
+}
+public class MultiButton : Selectable, IPointerClickHandler, IEventSystemHandler, ISubmitHandler
+{
+    [SerializeField] public List<MultiButtonAction> actions;
+    [SerializeField] public TextMeshProUGUI buttonName;
+    private int currentActionIdx = 0;
+    public void AddAction(string name, Action onClick)
+    {
+        var action = new MultiButtonAction() { name = name };
+        action.onClick.AddListener(() => onClick?.Invoke());
+        actions.Add(action);
+    }
+
+    public void Set(int idx)
+    {
+        currentActionIdx = 0;
+        OnClick();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick();
+    }
+    private void OnClick()
+    {
+        var currentAction = actions[currentActionIdx];
+        buttonName.text = currentAction.name;
+        currentAction.onClick?.Invoke();
+
+
+        currentActionIdx++;
+        if (currentActionIdx >= actions.Count) currentActionIdx = 0;
+    }
+
+    public void OnSubmit(BaseEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+}

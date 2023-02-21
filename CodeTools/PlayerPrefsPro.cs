bc3425753afcca9d6be6142410af8a9b0dbc1fd3
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-02-21 22:19:17 +0200

    remove debug log

--- CodeTools/PlayerPrefsPro.cs
+++ CodeTools/PlayerPrefsPro.cs
@@ -24,7 +24,6 @@ namespace Tools.PlayerPrefs
         private static void Set<T>(string key, T obj, bool saveKey)
         {
             var secretKey = GetKey(key);
-            Debug.LogError(JsonUtility.ToJson(new Json<T>(obj)));
             SetBytes(System.Text.Encoding.Default.GetBytes(JsonUtility.ToJson(new Json<T>(obj))), secretKey);
             if (saveKey) SetNewKey(secretKey);
         }

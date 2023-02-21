625b046beadc66c10009257922eb9e00c246dc62
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-02-21 20:31:12 +0200

    fix task tools null ref

--- CodeTools/TaskTools.cs
+++ CodeTools/TaskTools.cs
@@ -60,7 +60,7 @@ public static class TaskTools
             IsStopped = true;
         }
     }
-    public static TaskController taskController;
+    public static readonly TaskController taskController = new TaskController();
     public static Task WaitForSeconds(float value, bool playInEditorMode = true) => WaitForMilliseconds((long)value * 1000);
     public static Task WaitForMilliseconds(long value, bool playInEditorMode = true) => taskController.WaitForMilliseconds(value);
     public static async void Wait(float time, Action waitEvent)

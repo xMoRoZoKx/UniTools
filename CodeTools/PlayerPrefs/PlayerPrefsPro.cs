891981c36ed0fa60d9998ad0f3dda5b41194a32d
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-03-22 23:41:47 +0200

    fix player prefs pro keys

--- CodeTools/PlayerPrefs/PlayerPrefsPro.cs
+++ CodeTools/PlayerPrefs/PlayerPrefsPro.cs
@@ -19,6 +19,7 @@ namespace Tools.PlayerPrefs
         }
         private const string BASE_SAVE_KEY = "MJN3S";
         public static string Patch => Application.persistentDataPath + "/" + BASE_SAVE_KEY;
+        public static string GetKey(string key) => key.Trim('/');
         public static void Set<T>(string key, T obj) => Set<T>(key, obj, true);
         private static void Set<T>(string key, T obj, bool saveKey)
         {
@@ -27,6 +28,7 @@ namespace Tools.PlayerPrefs
         }
         public static T Get<T>(string key)
         {
+            key = GetKey(key);
             var bytes = GetBytes(key);
 
             string json = bytes == null ? "" : System.Text.Encoding.Default.GetString(bytes);
@@ -36,31 +38,36 @@ namespace Tools.PlayerPrefs
         }
         public static void SetBytes(this byte[] bytes, string key)
         {
+            key = GetKey(key);
             File.WriteAllBytes(Patch + key, bytes);
         }
         public static byte[] GetBytes(string key)
         {
+            key = GetKey(key);
             if (!HasKey(key)) return null;
             return File.ReadAllBytes(Patch + key);
         }
         public static string GetPatch(string key)
         {
+            key = GetKey(key);
             return Patch + key;
         }
 
-        public static void SetSprite(string key, Sprite sprite) => File.WriteAllBytes(Patch + key, sprite.texture.EncodeToPNG());
-        public static void SetTexture(string key, Texture2D texture) => File.WriteAllBytes(Patch + key, texture.EncodeToPNG());
-        public static void SetFloat(string key, float value) => Set(key, value);
-        public static void SetInt(string key, int value) => Set(key, value);
-        public static void SetString(string key, string value) => Set(key, value);
+        public static void SetSprite(string key, Sprite sprite) => File.WriteAllBytes(Patch + GetKey(key), sprite.texture.EncodeToPNG());
+        public static void SetTexture(string key, Texture2D texture) => File.WriteAllBytes(Patch + GetKey(key), texture.EncodeToPNG());
+        public static void SetFloat(string key, float value) => Set(GetKey(key), value);
+        public static void SetInt(string key, int value) => Set(GetKey(key), value);
+        public static void SetString(string key, string value) => Set(GetKey(key), value);
 
         public static Sprite GetSprite(string key)
         {
+            key = GetKey(key);
             var texture = GetTexture(key);
             return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), 100);
         }
         public static Texture2D GetTexture(string key)
         {
+            key = GetKey(key);
             string patch = Patch + key;
             if (!File.Exists(patch)) return null;
             Texture2D texture = Texture2D.normalTexture;
@@ -68,11 +75,11 @@ namespace Tools.PlayerPrefs
             texture.LoadImage(File.ReadAllBytes(patch));
             return texture;
         }
-        public static float GetFloat(string key) => Get<float>(key);
-        public static int GetInt(string key) => Get<int>(key);
-        public static string GetString(string key) => Get<string>(key);
+        public static float GetFloat(string key) => Get<float>(GetKey(key));
+        public static int GetInt(string key) => Get<int>(GetKey(key));
+        public static string GetString(string key) => Get<string>(GetKey(key));
 
-        public static bool HasKey(string key) => File.Exists(Patch + key);
+        public static bool HasKey(string key) => File.Exists(Patch + GetKey(key));
 
         private static List<string> GetAllKeys()
         {
@@ -88,6 +95,7 @@ namespace Tools.PlayerPrefs
         }
         public static void DeleteKey(string key)
         {
+            key = GetKey(key);
             if (!HasKey(key)) return;
             var keys = GetAllKeys();
             File.Delete(Patch + keys.Find(k => k == key));

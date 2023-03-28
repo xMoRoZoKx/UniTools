d6a692b50f8b607c5c54712c8123c931e968bd48
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-03-28 21:29:42 +0300

    add get random point mhetod

--- CodeTools/OtherTools/RandomTools.cs
+++ CodeTools/OtherTools/RandomTools.cs
@@ -1,4 +1,6 @@
 using System;
+using UnityEngine;
+
 namespace Tools
 {
     public static class RandomTools
@@ -8,5 +10,13 @@ namespace Tools
             if (chance == 0) return;
             if (UnityEngine.Random.Range(0, 100) <= chance) action?.Invoke();
         }
+
+        public static Vector3 GetRandomPointInSphere(Vector3 centre, float range, bool useX = true, bool useY = true, bool useZ = true)
+        {
+            return new Vector3(centre.x + GetRandom(), centre.y + GetRandom(), centre.z + GetRandom());
+
+            float GetRandom() => UnityEngine.Random.Range(-range, range);
+        }
+
     }
 }

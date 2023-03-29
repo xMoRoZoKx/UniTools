cdb201401585dc09acf70e77dae255399b7a3e3a
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-03-30 02:46:18 +0300

    fix singleton

--- CodeTools/OtherTools/RandomTools.cs
+++ CodeTools/OtherTools/RandomTools.cs
@@ -11,7 +11,7 @@ namespace Tools
             if (UnityEngine.Random.Range(0, 100) <= chance) action?.Invoke();
         }
 
-        public static Vector3 GetRandomPointInSphere(Vector3 centre, float range, bool useX = true, bool useY = true, bool useZ = true)
+        public static Vector3 GetRandomPointInRange(Vector3 centre, float range, bool useX = true, bool useY = true, bool useZ = true)
         {
             return new Vector3(centre.x + GetRandom(), centre.y + GetRandom(), centre.z + GetRandom());
 

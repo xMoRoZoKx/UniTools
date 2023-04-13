56bbe112ce968de88a84c7f0dc75c3d8b6204f7a
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-04-13 13:03:16 +0300

    add line fader

---
+++ UITools/Components/LineFader.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Tools;
+using UnityEngine;
+public class LineFader : MonoBehaviour
+{
+    [SerializeField] private LayerMask cullingMask;
+    [SerializeField] private Material cullingMaterial;
+    [SerializeField] private float cameraRadius = 2;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+    private RayCaster rayCaster = new();
+    private void Start()
+    {
+        Material[] lastCullingMaterials = null;
+        List<Material> cullingList = new();
+        rayCaster.OnRayEnter += hit =>
+        {
+            var meshRenderer = hit.transform?.GetComponent<MeshRenderer>();
+            if (meshRenderer == null) return;
+            cullingList.Resize(meshRenderer.materials.Length, cullingMaterial);
+            lastCullingMaterials = meshRenderer.materials;
+            meshRenderer.materials = cullingList.ToArray();
+        };
+        rayCaster.OnRayExit += hit =>
+        {
+            var meshRenderer = hit.transform.GetComponent<MeshRenderer>();
+            if (meshRenderer == null || lastCullingMaterials == null) return;
+            meshRenderer.materials = lastCullingMaterials;
+        };
+    }
+    private void Update()
+    {
+        if (rayCaster.ColliderExist(startPoint.position, endPoint.position, cullingMask)) rayCaster.Cast(startPoint.position, endPoint.position, cullingMask);
+        else rayCaster.Cast(endPoint.position, startPoint.position, cullingMask);
+        // rayCaster.Cast(startPos +, endPos, cullingMask);
+    }
+}

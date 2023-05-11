f63d8771f344a32233fb5363670afcd07e649455
xMoRoZoKx <105913669+xMoRoZoKx@users.noreply.github.com>
2023-05-11 23:55:31 +0300

    add some mhetods

--- CodeTools/OtherTools/RandomTools.cs
+++ CodeTools/OtherTools/RandomTools.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 
 namespace Tools
 {
+    public static class ColorTools
+    {
+        public static Color WithAlpha(this Color color, float alpha) => new Color() { r = color.r, g = color.g, b = color.b, a = alpha };
+    }
     public static class RandomTools
     {
         public static void InvokWithChance(Action action, int chance)
         {
             if (chance == 0) return;
-            if (UnityEngine.Random.Range(0, 100) <= chance) action?.Invoke();
+            if (GetChance(chance)) action?.Invoke();
         }
-
+        public static bool GetChance(int chance) => UnityEngine.Random.Range(0, 100) <= chance;
         public static Vector3 GetRandomPointInRange(Vector3 centre, float range, bool useX = true, bool useY = true, bool useZ = true)
         {
             return new Vector3(centre.x + (useX ? GetRandom() : 0), centre.y + (useY ? GetRandom() : 0), centre.z + (useZ ? GetRandom() : 0));

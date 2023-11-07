b5cfe0a754d0b717342a1887140ca9f19949ef0d
Ihor Moroz <105913669+xMoRoZoKx@users.noreply.github.com>
2023-11-07 20:32:05 +0200

    fix save layers

--- CodeTools/PlayerPrefs/AutoSaver.cs
+++ CodeTools/PlayerPrefs/AutoSaver.cs
@@ -4,17 +4,24 @@ using UniTools.PlayerPrefs;
 
 namespace UniTools.Reactive
 {
+
+    public enum SaveLayers
+    {
+        Default,
+        Layer1,
+        Layer2
+    }
     [System.Serializable]
     public class AutoSaver<T> : Reactive<T>, IAutoSaver<T>
     {
         public string key { get; private set; }
         public string layer { get; private set; }
-        public AutoSaver(string key, string layer = PlayerPrefsPro.BASE_LAYER)
+        public AutoSaver(string key, SaveLayers saveLayer = SaveLayers.Default)
         {
             this.key = key;
             this.ConnectToSaver(key, layer);
         }
-        public AutoSaver(string key, T defaultValue, string layer = PlayerPrefsPro.BASE_LAYER)
+        public AutoSaver(string key, T defaultValue, SaveLayers saveLayer = SaveLayers.Default)
         {
             if (!PlayerPrefsPro.HasKey(key, layer)) value = defaultValue;
 
